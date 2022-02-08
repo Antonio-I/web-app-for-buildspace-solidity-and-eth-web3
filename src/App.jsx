d96f15323b64dfaebb46eadbb3e53fba054531f3
Antonio Iuliano <antonio.iuliano@hotmail.com>
2022-02-08 23:56:08 +0000

    Check if wallet connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
+import { useEffect } from "react";
 
 export default function App() {
+  const checkWalletConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Requires metamask");
+    } else{
+      console.log("We have ethereum object", ethereum);
+    }
+  }
 
   const ideaAction = () => {
     
   }
+
+  useEffect(() => {
+    checkWalletConnected();
+  }, [])
   
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         💡 Idea Central
